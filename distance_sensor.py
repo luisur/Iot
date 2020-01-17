32b4bd52652ba4b1b1727682fbffebdb4c3a076e
luisur <luis.urre@opendeusto.es>
2020-01-17 09:50:45 +0000

    commit2

--- distance_sensor.py
+++ distance_sensor.py
@@ -1,12 +1,34 @@
 # Getting the libraries we need
+from __future__ import print_function
+import time
 from gpiozero import DistanceSensor
 from time import sleep
 from grove.gpio import GPIO
+from mraa import getGpioLookup
 from grove.grove_led import GroveLed
+from upm import pyupm_buzzer as upmBuzzer
+from twython import Twython
+from auth import (
+    consumer_key,
+    consumer_secret,
+    access_token,
+    access_token_secret
+)
+twitter = Twython(
+    consumer_key,
+    consumer_secret,
+    access_token,
+    access_token_secret
+)
 
 # Initialize ultrasonic sensor
 sensor = DistanceSensor(trigger=18, echo=24)
 led = GPIO(16, GPIO.OUT)
+pin = 12
+mraa_pin = getGpioLookup("GPIO%02d" % pin)
+buzzer = upmBuzzer.Buzzer(mraa_pin)
+chords = [upmBuzzer.BUZZER_DO, upmBuzzer.BUZZER_DO, upmBuzzer.BUZZER_DO];
+
 
 while True:
 	# Wait 2 seconds
@@ -22,5 +44,16 @@ while True:
     
     if distance < 0.06:
         led.write(1)
+        message = "La basura esta llena 2"
+        twitter.update_status(status=message)
+        print("Tweeted: {}".format(message))
+        for chord_ind in range (0,3):
+        # play each note for a half second
+             print(buzzer.playSound(chords[chord_ind], 500000))
+             time.sleep(0.1)
+   
     else:
         led.write(0)
+
+
+
